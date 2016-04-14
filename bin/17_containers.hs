071acef9180c9e34db16225a7f89259a4c55288d
Peter Tseng <pht24@cornell.edu>
2016-04-14 10:18:38 +0000

    day 17: Use dynamic programming
    As noted in the comments, this is gratuitous. Running in 0.1 seconds was
    plenty fast enough (without dynamic programming). However, this speeds
    it up to less than 0.02 seconds. Given this, I can't not include it.

--- bin/17_containers.hs
+++ bin/17_containers.hs
@@ -1,15 +1,27 @@
 import AdventOfCode (readInputFile)
 
-import Data.List (subsequences)
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Prelude hiding (rem)
 
-ways :: [Int] -> [[Int]]
-ways weights = filter ((== 150) . sum) (subsequences weights)
+-- This dynamic programming is gratuitous.
+-- Brute-forcing through `subsequences` still runs in < 0.1 seconds.
+-- However, with dynamic programming we run in about 0.02 seconds.
+
+ways :: [Int] -> Int -> Int -> IntMap Int
+ways [] _ _                  = IntMap.empty
+ways _ 0 used                = IntMap.singleton used 1
+ways _ rem _ | rem < 0       = IntMap.empty
+ways [c] rem used | c == rem = IntMap.singleton (used + 1) 1
+ways [_] _ _                 = IntMap.empty
+ways (c:cs) remaining used   = IntMap.unionWith (+) waysWith waysWithout
+  where waysWith = ways cs remaining used
+        waysWithout = ways cs (remaining - c) (used + 1)
 
 main :: IO ()
 main = do
   s <- readInputFile
   let weights = map read (lines s)
-      validWays = ways weights
-      minLength = minimum (map length validWays)
-  print (length validWays)
-  print (length (filter ((== minLength) . length) validWays))
+      validWays = ways weights 150 0
+  print (sum (IntMap.elems validWays))
+  print (snd (IntMap.findMin validWays))
