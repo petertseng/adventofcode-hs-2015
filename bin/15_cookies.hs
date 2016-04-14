2b6afb0191d3afd764d707a5ab3a2c3bced001e0
Peter Tseng <pht24@cornell.edu>
2016-04-14 07:38:55 +0000

    day 15: Skip cookie if ingredient 2 makes it doomed
    Improves runtime from 0.8 seconds to 0.66 seconds

--- bin/15_cookies.hs
+++ bin/15_cookies.hs
@@ -19,17 +19,17 @@ score traits = if any (< 0) traits then 0 else product traits
 scaleBy :: Int -> Ingredient -> Ingredient
 scaleBy n (traits, cals) = (map (* n) traits, cals * n)
 
-limitOfFirst :: [[Int]] -> Int
+limitOfFirst :: [Int] -> Int -> [[Int]] -> Int
 -- Start from 1 not 0.
 -- My most negative ingredient is the only one with positive durability.
 -- So we need at least one of that ingredient.
-limitOfFirst (i1:is) = case takeWhile ok [1..100] of
+limitOfFirst base limit (i1:is) = case takeWhile ok [1..limit] of
   [] -> 0 -- Even 1 isn't OK?!
   l -> last l
   where bests = foldr1 (zipWith max) is
         ok = all (> 0) . scores
-        scores n = zipWith (+) (map (* n) i1) (map (* (100 - n)) bests)
-limitOfFirst [] = error "need at least one ingredient"
+        scores n = foldr (zipWith (+)) base [map (* n) i1, map (* (limit - n)) bests]
+limitOfFirst _ _ [] = error "need at least one ingredient"
 
 otherTraits :: Ingredient -> [Int]
 otherTraits = fst
@@ -62,9 +62,14 @@ main = do
   s <- readInputFile
   let ingredients = map parseIngredient (lines s)
       sorted = sortOn (minimum . fst) ingredients
-      limit = limitOfFirst (map otherTraits sorted)
+      limit = limitOfFirst (repeat 0) 100 (map otherTraits sorted)
       tries = case length ingredients of
-        4 -> [(a, b, c) | a <- [0..limit], b <- [0..(100-a)], c <- [0..(100-a-b)]]
+        4 -> [(a, b, c) | a <- [0..limit]
+                        , b <- [0..limitOfFirst
+                                     (map (* a) (otherTraits (head sorted)))
+                                     (100 - a)
+                                     (map otherTraits (tail sorted))]
+                        , c <- [0..(100-a-b)]]
         3 -> [(a, b, 100 - a - b) | a <- [0..100], b <- [0..(100-a)]]
         2 -> [(a, 100 - a, 0) | a <- [0..100]]
         _ -> error "can't handle ingredient list of size other than 2 through 4."
