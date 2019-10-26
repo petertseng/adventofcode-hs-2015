ed83ad80a370c82ae4c0940c1ddf49511657aa25
Peter Tseng <pht24@cornell.edu>
2019-10-26 22:16:25 +0000

    day 05: Use `count`, `all`, `any`, etc. instead of explicit recursion
    Okay, there's still explicit recursion in `repeatingPair` because it is
    either `any`+state or `fold`+early terminate, and didn't have a way to
    express that.
    
    The readability is better and performance doesn't appear to be
    noticeably affected. Shows how much I know about Haskell performance.

--- bin/05_nice_strings.hs
+++ bin/05_nice_strings.hs
@@ -3,27 +3,26 @@ import AdventOfCode (readInputFile)
 import qualified Data.Map as Map
 
 nice1 :: String -> Bool
-nice1 = n1 (0, False, '\0')
-  where n1 :: (Int, Bool, Char) -> String -> Bool
-        n1 (_, _, 'a') ('b':_) = False
-        n1 (_, _, 'c') ('d':_) = False
-        n1 (_, _, 'p') ('q':_) = False
-        n1 (_, _, 'x') ('y':_) = False
-        n1 (vs, pair, prev) (x:xs) = n1 (vs + vowel x, pair || prev == x, x) xs
-        n1 (vs, pair, _) [] = pair && vs >= 3
-        vowel c = if c `elem` "aeiou" then 1 else 0
+nice1 s = count (`elem` "aeiou") s >= 3 && not (any bad pairs) && any (uncurry (==)) pairs
+  where bad ('a', 'b') = True
+        bad ('c', 'd') = True
+        bad ('p', 'q') = True
+        bad ('x', 'y') = True
+        bad _          = False
+        pairs = zip s (drop 1 s)
 
 nice2 :: String -> Bool
-nice2 = n2 ((Map.empty, 0, False), '\0', '\0', False)
-  where n2 :: ((Map.Map (Char, Char) Int, Int, Bool), Char, Char, Bool) -> String -> Bool
-        n2 ((_, _, True), _, _, True) _ = True
-        n2 _ [] = False
-        n2 (pairs, a, b, aba) (x:xs) = n2 (add pairs b x, b, x, aba || a == x) xs
-        add (m, i, False) a b = case Map.lookup (a, b) m of
-          Just j | j + 2 <= i -> (m, i, True)
-          Just _ -> (m, i + 1, False)
-          Nothing -> (Map.insert (a, b) i m, i + 1, False)
-        add m@(_, _, True) _ _ = m
+nice2 s = aba && repeatingPair Map.empty (zip3 s (drop 1 s) [0, 1 ..])
+  where aba = any (uncurry (==)) (zip s (drop 2 s))
+        repeatingPair :: Map.Map (Char, Char) Int -> [(Char, Char, Int)] -> Bool
+        repeatingPair _ [] = False
+        repeatingPair pairs ((a, b, i):xs) = case Map.lookup (a, b) pairs of
+          Just j | j + 2 <= i -> True
+          Just _ -> repeatingPair pairs xs
+          Nothing -> repeatingPair (Map.insert (a, b) i pairs) xs
+
+count :: (a -> Bool) -> [a] -> Int
+count f = length . filter f
 
 main :: IO ()
 main = do
@@ -31,4 +30,3 @@ main = do
   let l = lines s
   print (count nice1 l)
   print (count nice2 l)
-  where count p = length . filter p
