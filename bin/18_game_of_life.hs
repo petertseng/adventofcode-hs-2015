5592ebf4258ef7ba9b3594db10f3811cd8df3572
Peter Tseng <pht24@cornell.edu>
2022-12-11 00:32:48 +0000

    day 18: Use activity list implementation
    I'm not very satisfied with the result since the code complexity has
    obviously gone up and the benefit is a bit disappointing. The original
    was about 0.75 seconds and this is about 0.58 seconds.

--- bin/18_game_of_life.hs
+++ bin/18_game_of_life.hs
@@ -1,51 +1,96 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 import AdventOfCode (readInputFile)
 
-import Control.Monad (when)
+import Control.Monad (foldM_, when)
 import Control.Monad.ST (ST, runST)
-import Data.Array.MArray (getElems, newListArray, readArray, writeArray)
+import Data.Array.MArray (getElems, modifyArray, newListArray, readArray, writeArray)
 import Data.Array.ST (STUArray)
+import Data.Bits (shiftL, testBit)
 import Data.Foldable (for_)
+import Data.Int (Int8)
+import Data.Maybe (catMaybes)
+import Data.Traversable (for)
 
-gameOfLife :: [[Bool]] -> Int -> Bool -> [Bool]
+gameOfLife :: [[Bool]] -> Int -> Bool -> [Int8]
 gameOfLife lights n cornersStuck = runST $ do
   let len = length lights
-  a <- newListArray ((0, 0), (len + 1, len + 1)) (pad lights) :: ST s (STUArray s (Int, Int) Bool)
-  b <- newListArray ((0, 0), (len + 1, len + 1)) (pad lights)
+  let maxXY = len - 1
+  a <- newListArray ((0, 0), (maxXY, maxXY)) (pack lights) :: ST s (STUArray s (Int, Int) Int8)
   when cornersStuck $
-    for_ [(y, x) | y <- [1, len], x <- [1, len]] $ \(y, x) ->
-      writeArray b (y, x) True
-  for_ [1..n] $ \i -> do
-    let (prev, next) = if even i then (a, b) else (b, a)
-    for_ [(y, x) | y <- [1..len], x <- [1..len]] $ \(y, x) -> do
-      let ds = [-1, 0, 1] :: [Int]
-          neighbours = [(y + dy, x + dx) | dy <- ds, dx <- ds, (dy, dx) /= (0, 0)]
-      neighborVals <- traverse (readArray prev) neighbours
-      val <- readArray prev (y, x)
-      let newValue = nextState neighborVals val
-      writeArray next (y, x) newValue
-    when cornersStuck $
-      for_ [(y, x) | y <- [1, len], x <- [1, len]] $ \(y, x) ->
-        writeArray next (y, x) True
-  -- 100 iterations is even, so we would have written to b.
-  getElems b
+    for_ [(y, x) | y <- [0, maxXY], x <- [0, maxXY]] $ \(y, x) ->
+      writeArray a (y, x) 3
 
-nextState :: [Bool] -> Bool -> Bool
-nextState neighbours val = case (countTrues neighbours, val) of
-  (n, _) | n < 2 -> False
-  (2, b)         -> b
-  (3, _)         -> True
-  _              -> False -- (n > 3)
+  -- Initial setup: Count neighbours.
+  for_ [(y, x) | y <- [0..maxXY], x <- [0..maxXY]] $ \(y, x) -> do
+    neighbourVals <- traverse (readArray a) (neighbours maxXY (y, x))
+    modifyArray a (y, x) (+ shiftL (fromIntegral (countAlive neighbourVals)) 2)
 
-pad :: [[Bool]] -> [Bool]
-pad rows = concat (surround emptyRow (map padRow rows))
-  where emptyRow = replicate (length rows + 2) False
-        padRow = surround False
+  let allCells = [(y, x) | y <- [0..maxXY], x <- [0..maxXY]]
 
-surround :: a -> [a] -> [a]
-surround x xs = x : xs ++ [x]
+  let step actives _ = do
+        changed <- for actives $ \(y, x) -> do
+          val <- readArray a (y, x)
+          let isCorner = (x == 0 || x == maxXY) && (y == 0 || y == maxXY)
+              isAlive = cornersStuck && isCorner || 10 <= val && val <= 15
+          if isAlive /= alive val
+            then return (Just (y, x))
+            else do
+              writeArray a (y, x) (val - 1)
+              return Nothing
 
-countTrues :: [Bool] -> Int
-countTrues = length . filter id
+        let changed' = catMaybes changed
+
+        newActives <- for changed' $ \(y, x) -> do
+          val <- readArray a (y, x)
+          let delta = if alive val then -4 else 4
+          newThisCell <- for (neighboursAndSelf maxXY (y, x)) $ \(ny, nx) -> do
+            nval <- readArray a (ny, nx)
+            if even nval
+              then do
+                writeArray a (ny, nx) (nval + delta + 1)
+                return (Just (ny, nx))
+              else do
+                writeArray a (ny, nx) (nval + delta)
+                return Nothing
+
+          modifyArray a (y, x) (subtract (delta `div` 2))
+          return (catMaybes newThisCell)
+
+        return (changed' ++ concat newActives)
+
+  foldM_ step allCells [1..n]
+  getElems a
+
+neighbours :: Int -> (Int, Int) -> [(Int, Int)]
+neighbours maxXY (y, x) =
+  [(ny, nx) | ny <- yrange, nx <- xrange, (ny, nx) /= (y, x)]
+  where (yrange, xrange) = ranges maxXY y x
+
+-- The duplication is regrettable, but does save some time.
+neighboursAndSelf :: Int -> (Int, Int) -> [(Int, Int)]
+neighboursAndSelf maxXY (y, x) = [(ny, nx) | ny <- yrange, nx <- xrange]
+  where (yrange, xrange) = ranges maxXY y x
+
+ranges :: Int -> Int -> Int -> ([Int], [Int])
+ranges maxXY y x = ([yMin..yMax], [xMin..xMax])
+  where yMin = max (y - 1) 0
+        yMax = min (y + 1) maxXY
+        xMin = max (x - 1) 0
+        xMax = min (x + 1) maxXY
+
+-- Packs lights into their byte form:
+-- bit 0 is active/inactive
+-- bit 1 is alive/dead
+-- bits 2+ are neighbour count
+pack :: [[Bool]] -> [Int8]
+pack = concatMap (map (\b -> if b then 3 else 1))
+
+alive :: Int8 -> Bool
+alive = flip testBit 1
+
+countAlive :: [Int8] -> Int
+countAlive = length . filter alive
 
 toLight :: Char -> Bool
 toLight '#' = True
@@ -56,5 +101,5 @@ main :: IO ()
 main = do
   s <- readInputFile
   let rows = map (map toLight) (lines s)
-  print (countTrues (gameOfLife rows 100 False))
-  print (countTrues (gameOfLife rows 100 True))
+  print (countAlive (gameOfLife rows 100 False))
+  print (countAlive (gameOfLife rows 100 True))
