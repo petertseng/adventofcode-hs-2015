petertseng/adventofcode-hs

day 18: Blindly look through all cells rather than activity list
It appears to be too much of a burden to keep up the activity list, as
removing it improves runtime: 0.57 seconds -> 0.38 seconds.

bin/18_game_of_life.hs
@@ -2,11 +2,11 @@
 
 import AdventOfCode (readInputFile)
 
-import Control.Monad (foldM_, forM, forM_, when)
+import Control.Monad (forM, forM_, when)
 import Data.Array.IArray (elems)
 import Data.Array.MArray (newListArray, readArray, writeArray)
 import Data.Array.ST (runSTUArray)
-import Data.Bits (shiftL, testBit)
+import Data.Bits (shiftL)
 import Data.Int (Int8)
 import Data.Maybe (catMaybes)
 
@@ -17,49 +17,35 @@ gameOfLife lights n cornersStuck = elems $ runSTUArray $ do
   a <- newListArray ((0, 0), (maxXY, maxXY)) (pack lights)
   when cornersStuck $
     forM_ [(y, x) | y <- [0, maxXY], x <- [0, maxXY]] $ \(y, x) ->
-      writeArray a (y, x) 3
-
-  -- Initial setup: Count neighbours.
-  forM_ [(y, x) | y <- [0..maxXY], x <- [0..maxXY]] $ \(y, x) -> do
-    neighbourVals <- mapM (readArray a) (neighbours maxXY (y, x))
-    val <- readArray a (y, x)
-    writeArray a (y, x) (val + shiftL (fromIntegral (countAlive neighbourVals)) 2)
+      writeArray a (y, x) 1
 
   let allCells = [(y, x) | y <- [0..maxXY], x <- [0..maxXY]]
 
-  let step actives _ = do
-        changed <- forM actives $ \(y, x) -> do
-          val <- readArray a (y, x)
-          let isCorner = (x == 0 || x == maxXY) && (y == 0 || y == maxXY)
-              isAlive = cornersStuck && isCorner || 10 <= val && val <= 15
-          if isAlive /= alive val
-            then return (Just (y, x))
-            else do
-              writeArray a (y, x) (val - 1)
-              return Nothing
+  -- Initial setup: Count neighbours.
+  forM_ allCells $ \(y, x) -> do
+    neighbourVals <- mapM (readArray a) (neighbours maxXY (y, x))
+    val <- readArray a (y, x)
+    writeArray a (y, x) (val + shiftL (fromIntegral (countAlive neighbourVals)) 1)
 
-        let changed' = catMaybes changed
+  forM_ [1..n] $ \_ -> do
+    changed <- forM allCells $ \(y, x) -> do
+      val <- readArray a (y, x)
+      let isCorner = (x == 0 || x == maxXY) && (y == 0 || y == maxXY)
+          isAlive = cornersStuck && isCorner || 5 <= val && val <= 7
+      if isAlive /= alive val
+        then return (Just (y, x))
+        else return Nothing
 
-        newActives <- forM changed' $ \(y, x) -> do
-          val <- readArray a (y, x)
-          let delta = if alive val then -4 else 4
-          newThisCell <- forM (neighboursAndSelf maxXY (y, x)) $ \(ny, nx) -> do
-            nval <- readArray a (ny, nx)
-            if even nval
-              then do
-                writeArray a (ny, nx) (nval + delta + 1)
-                return (Just (ny, nx))
-              else do
-                writeArray a (ny, nx) (nval + delta)
-                return Nothing
+    forM_ (catMaybes changed) $ \(y, x) -> do
+      val <- readArray a (y, x)
+      let delta = if alive val then -2 else 2
+      forM_ (neighboursAndSelf maxXY (y, x)) $ \(ny, nx) -> do
+        nval <- readArray a (ny, nx)
+        writeArray a (ny, nx) (nval + delta)
 
-          val' <- readArray a (y, x)
-          writeArray a (y, x) (val' - (delta `div` 2))
-          return (catMaybes newThisCell)
+      val' <- readArray a (y, x)
+      writeArray a (y, x) (val' - (delta `div` 2))
 
-        return (changed' ++ concat newActives)
-
-  foldM_ step allCells [1..n]
   return a
 
 neighbours :: Int -> (Int, Int) -> [(Int, Int)]
@@ -80,14 +66,13 @@ ranges maxXY y x = ([yMin..yMax], [xMin..xMax])
         xMax = min (x + 1) maxXY
 
 -- Packs lights into their byte form:
--- bit 0 is active/inactive
--- bit 1 is alive/dead
--- bits 2+ are neighbour count
+-- bit 0 is alive/dead
+-- bits 1+ are neighbour count
 pack :: [[Bool]] -> [Int8]
-pack = concatMap (map (\b -> if b then 3 else 1))
+pack = concatMap (map (\b -> if b then 1 else 0))
 
 alive :: Int8 -> Bool
-alive = flip testBit 1
+alive = odd
 
 countAlive :: [Int8] -> Int
 countAlive = length . filter alive

petertseng/adventofcode-hs-2015

bin/18_game_of_life.hs
@@ -2,11 +2,11 @@
 
 import AdventOfCode (readInputFile)
 
-import Control.Monad (foldM_, when)
+import Control.Monad (when)
 import Data.Array.IArray (elems)
 import Data.Array.MArray (modifyArray, newListArray, readArray, writeArray)
 import Data.Array.ST (runSTUArray)
-import Data.Bits (shiftL, testBit)
+import Data.Bits (shiftL)
 import Data.Foldable (for_)
 import Data.Int (Int8)
 import Data.Maybe (catMaybes)
@@ -19,47 +19,33 @@ gameOfLife lights n cornersStuck = elems $ runSTUArray $ do
   a <- newListArray ((0, 0), (maxXY, maxXY)) (pack lights)
   when cornersStuck $
     for_ [(y, x) | y <- [0, maxXY], x <- [0, maxXY]] $ \(y, x) ->
-      writeArray a (y, x) 3
-
-  -- Initial setup: Count neighbours.
-  for_ [(y, x) | y <- [0..maxXY], x <- [0..maxXY]] $ \(y, x) -> do
-    neighbourVals <- traverse (readArray a) (neighbours maxXY (y, x))
-    modifyArray a (y, x) (+ shiftL (fromIntegral (countAlive neighbourVals)) 2)
+      writeArray a (y, x) 1
 
   let allCells = [(y, x) | y <- [0..maxXY], x <- [0..maxXY]]
 
-  let step actives _ = do
-        changed <- for actives $ \(y, x) -> do
-          val <- readArray a (y, x)
-          let isCorner = (x == 0 || x == maxXY) && (y == 0 || y == maxXY)
-              isAlive = cornersStuck && isCorner || 10 <= val && val <= 15
-          if isAlive /= alive val
-            then return (Just (y, x))
-            else do
-              writeArray a (y, x) (val - 1)
-              return Nothing
+  -- Initial setup: Count neighbours.
+  for_ allCells $ \(y, x) -> do
+    neighbourVals <- traverse (readArray a) (neighbours maxXY (y, x))
+    val <- readArray a (y, x)
+    writeArray a (y, x) (val + shiftL (fromIntegral (countAlive neighbourVals)) 1)
 
-        let changed' = catMaybes changed
+  for_ [1..n] $ \_ -> do
+    changed <- for allCells $ \(y, x) -> do
+      val <- readArray a (y, x)
+      let isCorner = (x == 0 || x == maxXY) && (y == 0 || y == maxXY)
+          isAlive = cornersStuck && isCorner || 5 <= val && val <= 7
+      if isAlive /= alive val
+        then return (Just (y, x))
+        else return Nothing
 
-        newActives <- for changed' $ \(y, x) -> do
-          val <- readArray a (y, x)
-          let delta = if alive val then -4 else 4
-          newThisCell <- for (neighboursAndSelf maxXY (y, x)) $ \(ny, nx) -> do
-            nval <- readArray a (ny, nx)
-            if even nval
-              then do
-                writeArray a (ny, nx) (nval + delta + 1)
-                return (Just (ny, nx))
-              else do
-                writeArray a (ny, nx) (nval + delta)
-                return Nothing
+    for_ (catMaybes changed) $ \(y, x) -> do
+      val <- readArray a (y, x)
+      let delta = if alive val then -2 else 2
+      for_ (neighboursAndSelf maxXY (y, x)) $ \(ny, nx) -> do
+        modifyArray a (ny, nx) (+ delta)
 
-          modifyArray a (y, x) (subtract (delta `div` 2))
-          return (catMaybes newThisCell)
+      modifyArray a (y, x) (subtract (delta `div` 2))
 
-        return (changed' ++ concat newActives)
-
-  foldM_ step allCells [1..n]
   return a
 
 neighbours :: Int -> (Int, Int) -> [(Int, Int)]
@@ -80,14 +66,13 @@ ranges maxXY y x = ([yMin..yMax], [xMin..xMax])
         xMax = min (x + 1) maxXY
 
 -- Packs lights into their byte form:
--- bit 0 is active/inactive
--- bit 1 is alive/dead
--- bits 2+ are neighbour count
+-- bit 0 is alive/dead
+-- bits 1+ are neighbour count
 pack :: [[Bool]] -> [Int8]
-pack = concatMap (map (\b -> if b then 3 else 1))
+pack = concatMap (map (\b -> if b then 1 else 0))
 
 alive :: Int8 -> Bool
-alive = flip testBit 1
+alive = odd
 
 countAlive :: [Int8] -> Int
 countAlive = length . filter alive